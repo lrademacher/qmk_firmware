5db94347603219f8b6075207ca2299321cb880de
Lars Rademacher <rademacher.lars@gmx.de>
2022-03-01 23:20:59 +0100

    first full layout draft

--- keyboards/crkbd/keymaps/lrademacher/keymap.c
+++ keyboards/crkbd/keymaps/lrademacher/keymap.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
+#include "keymap_german.h"
 
 #define DEF 0 // Default layer
 #define SYM 1 // Symbols and navigation
@@ -27,11 +28,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DEF] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+       KC_TAB,    DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,                         DE_Z,    DE_U,    DE_I,    DE_O,   DE_P,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_HASH, XXXXXXX, 
+      KC_LSFT,    DE_A,    DE_S,    DE_D,    DE_F,    DE_G,                         DE_H,    DE_J,    DE_K,    DE_L, DE_HASH, XXXXXXX, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_MINS,  KC_ESC, 
+      KC_LCTL,    DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,                         DE_N,    DE_M, DE_COMM,  DE_DOT, DE_MINS,  KC_ESC, 
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT, MO(SYM),  KC_SPC,     KC_ENT, MO(NUM), KC_LGUI
                                       //`--------------------------'  `--------------------------'
@@ -40,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [SYM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       _______,XXXXXXX, KC_PGUP,   KC_UP, KC_PGDN, XXXXXXX,                      XXXXXXX, KC_LBRC, XXXXXXX, KC_SCLN, KC_PAUS, _______, 
+      _______,  KC_INS, KC_PGUP,   KC_UP, KC_PGDN, KC_PAUS,                      DE_EXLM,   DE_AT, DE_HASH,  DE_DLR, DE_PERC, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_QUOT, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, _______, 
+      _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END,                      DE_QUOT, DE_LPRN, DE_RPRN, DE_LBRC, DE_RBRC, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_HOME, XXXXXXX, KC_END,  XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+      _______, DE_ADIA, DE_ODIA, DE_UDIA,   DE_SS, KC_PSCR,                      XXXXXXX, DE_LCBR, DE_RCBR, DE_SLSH, DE_BSLS, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, MO(MSC), _______
                                       //`--------------------------'  `--------------------------'
@@ -52,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [NUM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_PPLS,   KC_P7,   KC_P8,   KC_P9,   KC_P0, _______, 
+      _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                      DE_PLUS,   KC_P7,   KC_P8,   KC_P9,   KC_P0, _______, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,                      KC_PMNS,   KC_P4,   KC_P5,   KC_P6, KC_PCMM, KC_PEQL, 
+      _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                      DE_MINS,   KC_P4,   KC_P5,   KC_P6, DE_SLSH,  DE_EQL, 
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_UNDS, KC_LCBR, KC_RCBR,  KC_GRV, KC_PSLS,                      KC_PAST,   KC_P1,   KC_P2,   KC_P3, KC_PDOT, KC_PENT,
+      _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12, DE_BSLS,                      DE_ASTR,   KC_P1,   KC_P2,   KC_P3,  DE_DOT,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, MO(MSC), _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
